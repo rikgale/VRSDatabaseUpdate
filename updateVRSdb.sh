70c1db6bdbac36acc5d39b07cfcb747811ec9563
rikgale <rikgale@hotmail.com>
2023-12-09 23:28:39 +0000

    update 2023-12-09 23:28:45

--- updateVRSdb.sh
+++ updateVRSdb.sh
@@ -21,11 +21,19 @@ base_station_jp_aircraft_table="JPAircraft"
 base_station_bj_aircraft_table="BJAircraft"
 icao_type_conversion_csv="$base_path/DatabaseUpdateFiles/ICAOTypeConversion.csv"
 icao_type_conversion_table_name="ICAOTypeConversion"
+icao_type_conversion_url="https://raw.githubusercontent.com/rikgale/ICAOList/main/ICAOTypeConversion.csv"
 mil_icao_op_lookup_csv="$base_path/DatabaseUpdateFiles/MilICAOOperatorLookUp.csv"
 mil_type_op_lookup_table_name="MilICAOOperatorLookup"
+mil_icao_op_lookup_url="https://raw.githubusercontent.com/rikgale/ICAOList/main/MilICAOOperatorLookUp.csv"
 os_db_path="$base_path/DatabaseUpdateFiles/aircraftDatabase.csv"
 os_db_table_name="OSkyData"
 download_os_db_csv="https://opensky-network.org/datasets/metadata/aircraftDatabase.csv"
+reg_prefix_list_url="https://raw.githubusercontent.com/rikgale/ICAOList/main/RegPrefixList.csv"
+reg_prefix_list_csv="$base_path/DatabaseUpdateFiles/RegPrefixList.csv"
+reg_prefix_list_table_name="RegPrefixList"
+pa_db_url="https://raw.githubusercontent.com/sdr-enthusiasts/plane-alert-db/main/plane_images.csv"
+pa_db_csv="$base_path/DatabaseUpdateFiles/plane_images.csv"
+pa_db_table_name="PlaneImages"
 
 # SQL file with variables
 update_database_sql="$base_path/DatabaseUpdateFiles/sql/create_temp_database.sql"
@@ -54,16 +62,40 @@ delete_if_exists() {
   fi
 }
 
+# Delete old ICAOList.csv
 delete_if_exists "$icao_list_file"
 
 # Download the latest ICAOList.csv from GitHub
 download_file "$icao_list_url" "$icao_list_file"
 
+# Delete old plane_images.csv
+delete_if_exists "$pa_db_csv"
 
-# Check if os_db_path exists and delete it if it does
+# Download the latest plane_images.csv from GitHub
+download_file "$pa_db_url" "$pa_db_csv"
+
+# Delete old RegPrefixList.csv
+delete_if_exists "$reg_prefix_list_csv"
+
+# Download the latest RegPrefixList.csv from GitHub
+download_file "$reg_prefix_list_url" "$reg_prefix_list_csv"
+
+# Delete old MilICAOperatorLookup.csv
+delete_if_exists "$mil_icao_op_lookup_csv"
+
+# Download the latest MilICAOperatorLookup.csv from GitHub
+download_file "$mil_icao_op_lookup_url" "$mil_icao_op_lookup_csv"
+
+# Delete Old ICAOTypeConversion.csv
+delete_if_exists "$icao_type_conversion_csv"
+
+# Download the latest ICAOTypeConversion.csv from GitHub
+download_file "$icao_type_conversion_url" "$icao_type_conversion_csv"
+
+# Check if aircraftDatabase.csv (OpenSky) exists and delete it if it does
 delete_if_exists "$os_db_path"
 
-# Download the CSV file using the download_file function
+# Download the aircraftDatabase.csv (OpenSky) using the download_file function
 download_file "$download_os_db_csv" "$os_db_path"
 
 # Check id modTemp.sqb exists and delete it if it does
@@ -99,6 +131,20 @@ if [ ! -e "$os_db_path" ]; then
   exit 1
 fi
 
+# Check if RegprefixList.csv exists
+if [ ! -e "$reg_prefix_list_csv" ]; then
+  log "Error: $reg_prefix_list_csv does not exist. Exiting script."
+  exit 1
+fi
+
+# Check if plane_images.csv exists
+if [ ! -e "$pa_db_csv" ]; then
+  log "Error: $pa_db_csv does not exist. Exiting script."
+  exit 1
+fi
+
+
+
 # Run SQLite commands with logging
 log "Starting SQLite commands."
 
@@ -132,6 +178,33 @@ CREATE TABLE IF NOT EXISTS "$icao_list_table_name" (
 -- Import data from ICAOList.csv into the new table
 .mode csv
 .import "$icao_list_file" "$icao_list_table_name"
+
+
+-- This section imports List of Registration Prefixes from Regprefixlist.csv
+-- Create a new table for ICAOList
+CREATE TABLE IF NOT EXISTS "$reg_prefix_list_table_name" (
+  Country TEXT,
+  RegPrefix TEXT,
+  Notes TEXT
+);
+
+-- Import data from Regprefixlist.csv into the new table
+.mode csv
+.import "$reg_prefix_list_csv" "$reg_prefix_list_table_name"
+
+
+-- This section imports List of Plane Images URL from plane_images.csv
+-- Create a new table for ICAOList
+CREATE TABLE IF NOT EXISTS "$pa_db_table_name" (
+  ModeS TEXT,
+  PictureURL1 TEXT,
+  PictureURL2 TEXT,
+  PictureURL3 TEXT
+);
+
+-- Import data from plane_iamges.csv into the new table
+.mode csv
+.import "$pa_db_csv" "$pa_db_table_name"
 
 
 -- This setion imports OpenSky data from downloaded file aircraftData.csv
