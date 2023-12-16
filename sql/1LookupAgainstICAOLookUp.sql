68d1394d2b21d22facc5ce3e2dd07b79763ec4fb
rikgale <rikgale@hotmail.com>
2023-12-16 16:17:40 +0000

    Updated 2023-12-16

--- sql/1LookupAgainstICAOLookUp.sql
+++ sql/1LookupAgainstICAOLookUp.sql
@@ -1,17 +1,43 @@
-UPDATE BJAircraft
-SET ICAOTypeCode = (
-    SELECT NewICAOTypeCode
-    FROM ICAOTypeConversion
-    WHERE ICAOTypeConversion.OldICAOTypeCode = BJAircraft.ICAOTypeCode
-)
-WHERE ICAOTypeCode IN (SELECT OldICAOTypeCode FROM ICAOTypeConversion);
-
 UPDATE JPAircraft
-SET ICAOTypeCode = (
+SET
+  ICAOTypeCode = (
     SELECT NewICAOTypeCode
     FROM ICAOTypeConversion
     WHERE ICAOTypeConversion.OldICAOTypeCode = JPAircraft.ICAOTypeCode
-)
+  ),
+  OperatorFlagCode = REPLACE(
+    OperatorFlagCode,
+    (
+      SELECT OldICAOTypeCode
+      FROM ICAOTypeConversion
+      WHERE ICAOTypeConversion.OldICAOTypeCode = JPAircraft.ICAOTypeCode
+    ),
+    (
+      SELECT NewICAOTypeCode
+      FROM ICAOTypeConversion
+      WHERE ICAOTypeConversion.OldICAOTypeCode = JPAircraft.ICAOTypeCode
+    )
+  )
 WHERE ICAOTypeCode IN (SELECT OldICAOTypeCode FROM ICAOTypeConversion);
 
-
+UPDATE BJAircraft
+SET
+  ICAOTypeCode = (
+    SELECT NewICAOTypeCode
+    FROM ICAOTypeConversion
+    WHERE ICAOTypeConversion.OldICAOTypeCode = BJAircraft.ICAOTypeCode
+  ),
+  OperatorFlagCode = REPLACE(
+    OperatorFlagCode,
+    (
+      SELECT OldICAOTypeCode
+      FROM ICAOTypeConversion
+      WHERE ICAOTypeConversion.OldICAOTypeCode = BJAircraft.ICAOTypeCode
+    ),
+    (
+      SELECT NewICAOTypeCode
+      FROM ICAOTypeConversion
+      WHERE ICAOTypeConversion.OldICAOTypeCode = BJAircraft.ICAOTypeCode
+    )
+  )
+WHERE ICAOTypeCode IN (SELECT OldICAOTypeCode FROM ICAOTypeConversion);
